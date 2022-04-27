2d8e4185e636dc713fe7804ad993e95e8e27507c
alattalatta <alattalatta@sorto.me>
2022-04-28 01:10:55 +0900

    Remove base/full rule separation

--- packages/eslint-config/full.js
+++
@@ -1,6 +0,0 @@
-const base = require('./index')
-
-module.exports = {
-  ...base,
-  extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking', ...base.extends],
-}
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     "plugin:prettier/recommended",
     "plugin:react-hooks/recommended",
   ],
